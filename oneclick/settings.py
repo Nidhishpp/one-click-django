e3ae6cbf62540c772b1c2de0a6f00183f5d83417
Nidhish PP <nidhishpp.pp@gmail.com>
2020-12-10 00:25:47 +0530

    image path update

--- oneclick/settings.py
+++ oneclick/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,3 +131,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
